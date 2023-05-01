7abd2db25ee475f14239ffa63a64b77fcefe1496
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2023-05-01 15:47:32 +0530

    Add files via upload

---
+++ sending_email.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.message import EmailMessage
+import ssl
+
+sender = 'megh.shah2003@gmail.com'
+password = 'rtgjtsqqzpmecpxk'
+receiver = input()
+
+subject = 'Test email'
+body = """
+Hey! I'm learning to send emails using python.
+"""
+
+test = EmailMessage()
+test['From'] = sender
+test['To'] = receiver
+test['Subject'] = subject
+test.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465, context=context) as smtp:
+    smtp.login(sender, password)
+    smtp.sendmail(sender, receiver, test.as_string())
